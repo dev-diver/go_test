5a303248d86af4d08973af23a78dd6c6008e41ab
dev-diver <valentine92exp@gmail.com>
2025-02-04 08:52:22 +0900

    테스트 작성 : Withdraw

--- pointers/pointer_test.go
+++ pointers/pointer_test.go
@@ -7,15 +7,31 @@ import (
 
 func TestWallet(t *testing.T) {
 
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
-	want := Bitcoin(20)
+		got := wallet.Balance()
+		want := Bitcoin(10)
 
-	fmt.Printf("address of wallet in test is %v\n", &wallet.balance)
+		fmt.Printf("address of wallet in test is %v\n", &wallet.balance)
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+
+	})
 }
