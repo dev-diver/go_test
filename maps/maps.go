acbfb25d67b59ef488f50eeb8f9be6957d5d0c39
dev-diver <valentine92exp@gmail.com>
2025-02-06 08:47:45 +0900

    make constant err

--- maps/maps.go
+++ maps/maps.go
@@ -1,13 +1,17 @@
 package maps
 
-import "errors"
-
 type Dictionary map[string]string
 
-var (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("word already exists")
+const (
+	ErrNotFound   = DictinaryErr("could not find the word you were looking for")
+	ErrWordExists = DictinaryErr("word already exists")
 )
+
+type DictinaryErr string
+
+func (e DictinaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 
