78296e203c80e138a8e8f617d8a394cee7f6dbc8
dev-diver <valentine92exp@gmail.com>
2025-02-06 07:47:42 +0900

    make small pass

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -3,7 +3,7 @@ package maps
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is a test"}
+	dictionary := map[string]string{"test": "this is just a test"}
 
 	got := Search(dictionary, "test")
 	want := "this is just a test"
