3ebd546ddb4cdefdd36d47fe62b68ebbbf6093e5
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2025-02-06 22:44:38 +0900

    configurable Sleeper Test

--- mock/mock.go
+++ mock/mock.go
@@ -44,6 +44,19 @@ func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
 const write = "write"
 const sleep = "sleep"
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
 func Countdown(w io.Writer, sleeper Sleeper) {
 	for i := countdonwStart; i > 0; i-- {
 		fmt.Fprintln(w, i)
