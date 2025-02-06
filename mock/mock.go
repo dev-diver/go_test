97f466d9a54179443190ece1565f47e843aa0ad9
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2025-02-06 22:04:41 +0900

    Sleeper 정의, spySleeper 정의, 의존성 주입한 테스트 코드

--- mock/mock.go
+++ mock/mock.go
@@ -4,14 +4,28 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const finalWorld = "Go!"
 const countdonwStart = 3
 
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 func Countdown(w io.Writer) {
 	for i := countdonwStart; i > 0; i-- {
 		fmt.Fprintln(w, i)
+		time.Sleep(1 * time.Second)
 	}
 	fmt.Fprint(w, finalWorld)
 }
