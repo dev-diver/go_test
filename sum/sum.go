04bd21faceb9ecf8ad88362a733730cfdcda87cd
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2025-02-09 12:58:46 +0900

    add test pass

--- sum/sum.go
+++ sum/sum.go
@@ -1,5 +1,11 @@
 package main
 
 func Sum(numbers [5]int) int {
-	return 0
+	sum := 0
+
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
 }
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -4,12 +4,25 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("sum the numbers", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("sum the numbers", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
