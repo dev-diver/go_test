bfeb7457091865d736c9f4a4ea45d3c106adee37
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2025-02-09 11:57:43 +0900

    add driver

--- acceptance/cmd/httpserver/main_test.go
+++ acceptance/cmd/httpserver/main_test.go
@@ -3,9 +3,11 @@ package main_test
 import (
 	"testing"
 
+	go_specs_greet "valentine.com/tdd/acceptance"
 	specifications "valentine.com/tdd/acceptance/specification"
 )
 
 func TestHTTPServer(t *testing.T) {
-	specifications.GreetSpecification(t, &HTTPServer{})
+	driver := go_specs_greet.Driver{BaseURL: "http://localhost:8080"}
+	specifications.GreetSpecification(t, driver)
 }
